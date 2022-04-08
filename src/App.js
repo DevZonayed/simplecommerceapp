5b5b505c6fc8753d7603bfe9147f96c03f997ce3
DevZonayed <zonayed320@gmail.com>
2022-04-09 02:37:39 +0600

    fast

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
     <div className="App">
+      <h1>Hello js</h1>
       <Router>
         <Routes>
           <Route />
