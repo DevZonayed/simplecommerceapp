3637322b5db5bdf1f94a7ba4f99ffb3f11bfd74b
DevZonayed <zonayed320@gmail.com>
2022-04-12 02:54:05 +0600

    complete

--- src/components/verify/Verify.js
+++ src/components/verify/Verify.js
@@ -1,5 +1,25 @@
-function set(key, data) {
+let cryprion = require("crypto-js");
+export function sentusertolocal(key, data) {
   if (key !== false && data !== false) {
-    localStorage.setItem(key, JSON.stringify(data));
+    localStorage.setItem(
+      key,
+      cryprion.AES.encrypt(
+        JSON.stringify(data),
+        "handleuserbysorobindu"
+      ).toString()
+    );
+  }
+}
+export function getusertolocal(key) {
+  let data = JSON.parse(
+    cryprion.AES.decrypt(
+      localStorage.getItem(key),
+      "handleuserbysorobindu"
+    ).toString(cryprion.enc.Utf8)
+  );
+  if (data) {
+    return data;
+  } else {
+    return false;
   }
 }
